99df9cbae47a76dc66d416e53d0ed4a4c1bf406f
GunHyung Ham <gdreagon@naver.com>
2022-05-12 04:44:15 +0900

    feat #3: 폰트 helvetica, Song-Myung, Nanum_Pen_Script 추가

--- presentation/src/main/java/com/teamsu/presentation/ui/theme/Type.kt
+++ presentation/src/main/java/com/teamsu/presentation/ui/theme/Type.kt
@@ -2,11 +2,25 @@ package com.teamsu.presentation.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.teamsu.presentation.R
 
 // Set of Material typography styles to start with
+val helvetica = FontFamily(
+    Font(R.font.helvetica_bold, FontWeight.Bold, FontStyle.Normal),
+)
+val songMyung = FontFamily(
+    Font(R.font.song_myung_regular, FontWeight.Normal, FontStyle.Normal),
+)
+val naNumPenScript = FontFamily(
+    Font(R.font.nanum_pen_script_regular, FontWeight.Normal, FontStyle.Normal)
+)
+
+
 val Typography = Typography(
     body1 = TextStyle(
         fontFamily = FontFamily.Default,
