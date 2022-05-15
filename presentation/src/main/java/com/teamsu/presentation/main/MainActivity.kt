14b08e55b3a08f651db5ef176039913cc5cf0e1b
GunHyung Ham <gdreagon@naver.com>
2022-05-15 18:38:39 +0900

    feat #5: MainActivity 일때 시스템 상단바 검정 설정

--- presentation/src/main/java/com/teamsu/presentation/main/MainActivity.kt
+++ presentation/src/main/java/com/teamsu/presentation/main/MainActivity.kt
@@ -3,13 +3,21 @@ package com.teamsu.presentation.main
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            val systemUiController = rememberSystemUiController()
+            SideEffect {
+                systemUiController.setStatusBarColor(
+                    color = Color.Black
+                )
+            }
+            MainScreen()
         }
     }
 }
