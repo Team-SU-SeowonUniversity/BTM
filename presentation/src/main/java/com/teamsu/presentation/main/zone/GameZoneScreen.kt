e800e5b4315c306f4f45b12ffc18d4773f462f3f
GunHyung Ham <gdreagon@naver.com>
2022-05-17 22:32:40 +0900

    feat #11: (PerceptActivity, PerceptScreen) -> (GameZoneActivity, GameZoneScreen) 으로 변경

---
+++ presentation/src/main/java/com/teamsu/presentation/main/zone/GameZoneScreen.kt
@@ -0,0 +1,98 @@
+package com.teamsu.presentation.main.zone
+
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.GridCells
+import androidx.compose.foundation.lazy.LazyVerticalGrid
+import androidx.compose.material.Divider
+import androidx.compose.material.IconButton
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.teamsu.presentation.R
+import com.teamsu.presentation.main.zone.components.GameCard
+import com.teamsu.presentation.ui.theme.BackgroundColor
+import com.teamsu.presentation.ui.theme.songMyung
+
+@OptIn(ExperimentalFoundationApi::class)
+@Preview
+@Composable
+internal fun GameZoneScreen() {
+    val gameName = listOf(stringResource(id = R.string.fit_a_fragmented_photo))
+    val gameThumbnail = listOf(painterResource(id = R.drawable.back))
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(BackgroundColor)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(top = 20.dp)
+        ) {
+            IconButton(
+                onClick = { }
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.back),
+                    contentDescription = "back button"
+                )
+            }
+
+            Spacer(modifier = Modifier.width(5.dp))
+
+            Text(
+                text =  stringResource(id = R.string.percept),
+                color = Color.White,
+                fontSize = 30.sp,
+                fontFamily = songMyung,
+                fontWeight = FontWeight.Normal,
+            )
+        }
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        Divider(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 15.dp, end = 15.dp),
+            thickness = 2.dp,
+            color = Color.Black,
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        LazyVerticalGrid(
+            cells = GridCells.Fixed(2),
+            contentPadding = PaddingValues(start = 15.dp, end = 15.dp),
+            verticalArrangement = Arrangement.spacedBy(20.dp),
+            horizontalArrangement = Arrangement.spacedBy(30.dp)
+        ) {
+            items(gameName.size) { index ->
+                GameCard(
+                    image = gameThumbnail[index],
+                    textValue = gameName[index]
+                )
+            }
+        }
+    }
+}
