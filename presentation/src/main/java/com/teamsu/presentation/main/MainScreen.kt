b880be11a652318d2214a26672c9ef9e5d9256b8
GunHyung Ham <gdreagon@naver.com>
2022-05-17 15:49:11 +0900

    feat #8: 접근제한자 internal 로 변경 및 컴포저블 단일화 및 로고 텍스트 buildAnnotatedString 적용 및 메인 버튼 LazyVerticalGrid 적용

--- presentation/src/main/java/com/teamsu/presentation/main/MainScreen.kt
+++ presentation/src/main/java/com/teamsu/presentation/main/MainScreen.kt
@@ -1,35 +1,44 @@
 package com.teamsu.presentation.main
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Button
-import androidx.compose.material.ButtonDefaults
+import androidx.compose.foundation.lazy.GridCells
+import androidx.compose.foundation.lazy.LazyVerticalGrid
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.teamsu.presentation.R
+import com.teamsu.presentation.main.components.CustomMainButton
 import com.teamsu.presentation.ui.theme.BackgroundColor
-import com.teamsu.presentation.ui.theme.MainButtonColor
 import com.teamsu.presentation.ui.theme.songMyung
 
-@Preview
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun MainScreen() {
+internal fun MainScreen() {
+    val buttonText = listOf(
+        stringResource(id = R.string.percept),
+        stringResource(id = R.string.memory),
+        stringResource(id = R.string.instinctive),
+        stringResource(id = R.string.calculation),
+        stringResource(id = R.string.analysis),
+        stringResource(id = R.string.state),
+    )
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -37,92 +46,47 @@ fun MainScreen() {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        Column {
-            MainLogoRow(
-                firstText = "뇌",
-                restText = "를"
-            )
-            MainLogoRow(
-                firstText = " 훈",
-                restText = "련하는"
-            )
-            MainLogoRow(
-                firstText = "  아",
-                restText = "침"
-            )
-        }
-        Spacer(modifier = Modifier.height(40.dp))
-        Column {
-            ButtonRow(
-                buttonText1 = stringResource(id = R.string.percept),
-                buttonText2 = stringResource(id = R.string.memory)
-            )
-            ButtonRow(
-                buttonText1 = stringResource(id = R.string.instinctive),
-                buttonText2 = stringResource(id = R.string.calculation)
-            )
-            ButtonRow(
-                buttonText1 = stringResource(id = R.string.analysis),
-                buttonText2 = stringResource(id = R.string.state)
-            )
-        }
-    }
-}
-
-@Composable
-fun ButtonRow(
-    buttonText1: String,
-    buttonText2: String,
-) {
-    Row {
-        CustomMainButton(buttonText1)
-        Spacer(modifier = Modifier.width(30.dp))
-        CustomMainButton(buttonText2)
-    }
-    Spacer(modifier = Modifier.height(20.dp))
-}
-
-@Composable
-fun CustomMainButton(
-    textValue: String,
-) {
-    Button(
-        onClick = { },
-        colors = ButtonDefaults.buttonColors(MainButtonColor),
-        modifier = Modifier
-            .clip(RoundedCornerShape(10.dp))
-            .width(130.dp)
-            .height(100.dp)
-    ) {
         Text(
-            text = textValue,
-            fontWeight = FontWeight.Normal,
-            fontSize = 35.sp,
-            fontFamily = songMyung,
-            color = Color.Black
-        )
-    }
-}
+            text = buildAnnotatedString {
+                withStyle(
+                    SpanStyle(
+                        fontWeight = FontWeight.Normal,
+                        fontSize = 45.sp,
+                    )
+                ) { append("뇌") }; append("를\n")
 
-@Composable
-fun MainLogoRow(
-    firstText: String,
-    restText: String
-) {
-    Row(verticalAlignment = Alignment.CenterVertically) {
-        Text(
-            text = firstText,
-            fontWeight = FontWeight.Normal,
-            fontSize = 45.sp,
-            fontFamily = songMyung,
-            color = Color.Black
-        )
-        Text(
-            text = restText,
+                withStyle(
+                    SpanStyle(
+                        fontWeight = FontWeight.Normal,
+                        fontSize = 45.sp,
+                    )
+                ) { append(" 훈") }; append("련하는\n")
+
+                withStyle(
+                    SpanStyle(
+                        fontWeight = FontWeight.Normal,
+                        fontSize = 45.sp,
+                    )
+                ) { append("  아") }; append("침")
+            },
             fontWeight = FontWeight.Light,
             fontSize = 33.sp,
             fontFamily = songMyung,
             color = Color.Black
         )
+
+        Spacer(modifier = Modifier.height(40.dp))
+
+        LazyVerticalGrid(
+            cells = GridCells.Fixed(2),
+            userScrollEnabled = false,
+            contentPadding = PaddingValues(start = 50.dp, end = 50.dp),
+            verticalArrangement = Arrangement.spacedBy(20.dp),
+            horizontalArrangement = Arrangement.spacedBy(30.dp)
+        ) {
+            items(buttonText.size) { index ->
+                CustomMainButton(textValue = buttonText[index])
+            }
+        }
     }
 }
