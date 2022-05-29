a0eaa62129af937a5c3c0fc3f0888bfa075c31fa
GunHyung Ham <gdreagon@naver.com>
2022-05-29 19:26:34 +0900

    feat #19: Ability Bar Chart 제작

--- presentation/src/main/java/com/teamsu/presentation/main/zone/StateScreen.kt
+++ presentation/src/main/java/com/teamsu/presentation/main/zone/StateScreen.kt
@@ -1,5 +1,6 @@
 package com.teamsu.presentation.main.zone
 
+import android.graphics.Point
 import android.net.Uri
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
@@ -9,13 +10,11 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Divider
 import androidx.compose.material.Text
@@ -27,30 +26,38 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
 import com.skydoves.landscapist.coil.CoilImage
 import com.teamsu.presentation.R
+import com.teamsu.presentation.main.zone.common.BarChart
 import com.teamsu.presentation.main.zone.components.BackWard
 import com.teamsu.presentation.ui.theme.BackgroundColor
 import com.teamsu.presentation.ui.theme.naNumPenScript
-
-@Preview
-@Composable
-fun preview() {
-    StateScreen(navController = rememberNavController())
-}
 
 @Composable
 fun StateScreen(navController: NavController) {
     val (imageUri, setImageUri) = remember {
         mutableStateOf<Uri?>(null)
     }
+    val ability = listOf(
+        stringResource(id = R.string.perception),
+        stringResource(id = R.string.memory),
+        stringResource(id = R.string.instinctive),
+        stringResource(id = R.string.calculation),
+        stringResource(id = R.string.analysis),
+    )
+    val point = listOf(
+        Point(75, 600),
+        Point(240, 600),
+        Point(408, 600),
+        Point(570, 600),
+        Point(733, 600),
+    )
 
     Column(
         modifier = Modifier
@@ -98,11 +105,17 @@ fun StateScreen(navController: NavController) {
             }
 
             Divider(
-                modifier = Modifier.fillMaxWidth().padding(top = 5.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 5.dp),
                 thickness = 2.dp,
                 color = Color.Black,
             )
         }
+
+        Spacer(modifier = Modifier.height(50.dp))
+
+        BarChart(ability = ability, point = point)
     }
 }
 
